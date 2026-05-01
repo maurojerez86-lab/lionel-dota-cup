7593c0cd30aed0b59b420d31b09feada407f9a70
maurojerez86-lab <maurojerez.86@gmail.com>
2026-05-01 19:33:05 -0300

    Update get-players.js

--- netlify/functions/get-players.js
+++ netlify/functions/get-players.js
@@ -1,11 +1,10 @@
-const { getStore } = require('@netlify/blobs');
-
 exports.handler = async (event, context) => {
   try {
-    const store = getStore('players');
-    const { blobs } = await store.list();
+    const { blobs } = context;
+    const store = blobs.getStore('players');
+    const { blobs: list } = await store.list();
     const players = await Promise.all(
-      blobs.map(async ({ key }) => {
+      list.map(async ({ key }) => {
         try { return await store.get(key, { type: 'json' }); } catch(e) { return null; }
       })
     );
